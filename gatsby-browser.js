50f1f4d4845302906b40cebabd01c25da66f98af
Aleix Ràmia <aleixramia@hotmail.com>
2020-05-17 16:57:17 +0200

    Updates

--- gatsby-browser.js
+++
@@ -1,3 +0,0 @@
-// custom typefaces
-import "typeface-libre-franklin"
-import("./src/styles/layout.scss")
--- src/components/ui.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-const UI = (props) => {return};
-
-const SectionHeader = ({title}) => (
-    <h3 className="section-title">{title}</h3>
-);
-export default UI;
-export {SectionHeader};
